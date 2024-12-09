67dc07d665c185015454dd10f82e80a32c4d0105
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-12-09 11:19:39 -0700

    style: Reformat UserAgents and UserProfile models with consistent indentation

--- src/models/UserProfile.ts
+++ src/models/UserProfile.ts
@@ -1,27 +1,27 @@
-import { Model, D1QB } from 'd1-orm';
+import { Model } from 'd1-orm';
 
 export interface IUserProfile {
-  id?: number;
-  created_at?: string;
-  updated_at?: string;
-  user_role: string;
-  account_index?: number;
-  stx_address: string;
-  bns_address?: string;
+	id?: number;
+	created_at?: string;
+	updated_at?: string;
+	user_role: string;
+	account_index?: number;
+	stx_address: string;
+	bns_address?: string;
 }
 
 export class UserProfile extends Model {
-  constructor(db: D1Database) {
-    super(db, 'user_profiles');
-  }
+	constructor(db: D1Database) {
+		super(db, 'user_profiles');
+	}
 
-  schema = {
-    id: { type: 'number', primary: true },
-    created_at: { type: 'string' },
-    updated_at: { type: 'string' },
-    user_role: { type: 'string', notNull: true },
-    account_index: { type: 'number' },
-    stx_address: { type: 'string', notNull: true, unique: true },
-    bns_address: { type: 'string' }
-  };
+	schema = {
+		id: { type: 'number', primary: true },
+		created_at: { type: 'string' },
+		updated_at: { type: 'string' },
+		user_role: { type: 'string', notNull: true },
+		account_index: { type: 'number' },
+		stx_address: { type: 'string', notNull: true, unique: true },
+		bns_address: { type: 'string' },
+	};
 }
