4e65d52bb3f242a1a8bd5a3bd79bfda0cfa177e3
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-12-11 15:16:23 -0700

    fix: the terminator

--- src/database/twitter-bot.sql
+++ src/database/twitter-bot.sql
@@ -11,13 +11,13 @@ CREATE TABLE x_bot_authors (
   author_id TEXT NOT NULL UNIQUE,
   realname TEXT,
   username TEXT
-)
+);
 
 -- conversation in API
 CREATE TABLE x_bot_threads (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE x_bot_tweets (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,7 +34,7 @@ CREATE TABLE x_bot_tweets (
   FOREIGN KEY (author_id) REFERENCES x_bot_authors(author_id) ON DELETE CASCADE,
   FOREIGN KEY (thread_id) REFERENCES x_bot_threads(id) ON DELETE CASCADE,
   FOREIGN KEY (parent_tweet_id) REFERENCES x_bot_tweets(tweet_id) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE x_bot_logs (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -45,4 +45,4 @@ CREATE TABLE x_bot_logs (
   log_message TEXT,
   -- pass info, error, etc
   FOREIGN KEY (tweet_id) REFERENCES x_bot_tweets(tweet_id) ON DELETE CASCADE
-)
+);
